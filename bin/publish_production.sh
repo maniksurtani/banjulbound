a5b6d14813758e0f122d4aa00f8901503fd8fe51
Manik Surtani <manik@squareup.com>
2013-11-17 23:53:00 -0800

    Fix scripts

--- bin/publish_production.sh
+++ bin/publish_production.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 rm -rf _site
-bin/fetch_docs.rb -f
 bundle exec awestruct -g -Pproduction --deploy
--- bin/publish_staging.sh
+++ bin/publish_staging.sh
@@ -3,6 +3,3 @@
 rm -rf _site
 bundle exec awestruct -g -Pstaging
 
-echo "**********************************************************"
-echo "Staging Build Complete: Now deploy using AppEngineLauncher"
-echo "**********************************************************"
